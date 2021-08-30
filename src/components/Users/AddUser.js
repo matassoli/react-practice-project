d782d9bc899303515f14b3ecb3a9b673006974aa
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-08-30 16:53:46 +0200

    add: user list

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,23 +11,43 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredName.trim().lenght === 0 || enteredAge.length === 0) {
+      return;
+    }
+
+    if (enteredAge < 1 || enteredAge.toString().length > 2) {
+      return;
+    }
     console.log(enteredName, enteredAge);
+    setEnteredName("");
+    setEnteredAge("");
   };
 
   const nameChangerHandler = (event) => {
     setEnteredName(event.target.value);
   };
   const ageChangerHandler = (event) => {
-    setEnteredAge(event.target.value);
+    setEnteredAge(+event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="name">Nome</label>
-        <input type="text" id="name" onChange={nameChangerHandler} />
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameChangerHandler}
+        />
         <label htmlFor="age">Età</label>
-        <input type="number" id="age" onChange={ageChangerHandler} />
+        <input
+          type="number"
+          id="age"
+          value={enteredAge}
+          onChange={ageChangerHandler}
+        />
         <Button type="submit">Aggiungi</Button>
       </form>
     </Card>
